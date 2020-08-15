9057e7b840c2a074366b9d6e5c088360aa0000b7
Manon <simon.li@gadz.org>
2020-08-16 00:30:01 +0200

    LOL

--- MusicPlayer.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var random = randomize()
-
-func _process(delta):
-	if $AudioStreamPlayer.playing == false :
-		var myMusic = randi()%8 + 1
-		$AudioStreamPlayer.stream = load("res://Assets/music/"+str(myMusic)+".ogg")
-		$AudioStreamPlayer.play()

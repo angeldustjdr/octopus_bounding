182c50a7380fd5541f1edfdb8a9e00063a4590f1
Georis <georis.billo@gmail.com>
2020-08-15 17:02:44 +0200

    Update GUI

--- Main.gd
+++ Main.gd
@@ -1,21 +1,23 @@
 extends Node
 
-export var money = 100
-export var reputation = 100
+export var day = 1
+export var money = 0
+export var reputation = 0
 export var compromised = 0
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_GUI()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func update_GUI():
+	get_node("GameArea/TimeArea/TimeLabel").text = "Day " + str(day)
 	get_node("GameArea/ScoreArea/Money/Money_label").text = str(money)
 	get_node("GameArea/ScoreArea/Reputation/Reputation_label").text = str(reputation)
-	get_node("GameArea/ScoreArea/Compromised/Compromised_label").text = str(compromised)
+	get_node("GameArea/ScoreArea/Compromised/Compromised_label").text = str(compromised) + "%"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(_delta):
